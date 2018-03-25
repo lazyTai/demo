c4fb059efe1858462a0ee06e2017f3ce676e55b0
lazyTai <1352983234@qq.com>
2018-03-25 17:04:16 +0800

    roolup 加入css

---
+++ rem/src/components/app.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="body">
+        app
+    </div>
+</template>
+<style scoped>
+.body {
+  background: black;
+}
+</style>
+
+<script>
+export default {};
+</script>
---
+++ rem/src/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue/dist/vue.js'
+import App from './components/app.vue'
+
+new Vue({
+    el: '#root',
+    template: "<App/>",
+    components: { App },
+})
