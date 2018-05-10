c1a7a8523526a59d01b63850107bc683a82c17d6
lazyTai <1352983234@qq.com>
2018-05-10 18:40:50 +0800

    git auto commit

--- webpack4/build/config/webpack.dev.js
+++ webpack4/build/config/webpack.dev.js
@@ -2,6 +2,9 @@ const path = require("path")
 var webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const argv = require('yargs').argv
+console.log(argv)
+console.log(argv.domain)
 module.exports = {
     entry: "./src/index.js",
     output: {
