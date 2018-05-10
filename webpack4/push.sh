812799e32409a068e444ad08c5e3b1cf69999cc9
lazyTai <1352983234@qq.com>
2018-05-10 18:02:53 +0800

    git auto commit

--- webpack4/push.sh
+++ webpack4/push.sh
@@ -1,9 +1,9 @@
-git config --global credential.helper store
-git pull
+# git config --global credential.helper store
+# git pull
 git add build/
 git add .
 git commit -a -m 'git auto commit'
-git log -1
+# git log -1
 git remove add origin https://github.com/lazyTai/demo.git
 #git push -u origin master
 git push
