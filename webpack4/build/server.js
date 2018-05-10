f5596a200a6ef95e5de5c5774c051fd133acb615
lazyTai <1352983234@qq.com>
2018-05-10 18:02:24 +0800

    git auto commit

--- webpack4/build/server.js
+++ webpack4/build/server.js
@@ -3,7 +3,7 @@ var app = express()
 
 app.use('/', express.static('./dist'));
 app.get('/index', function (req, res) {
-    return res.json("this is my server and code")
+    return res.json("this is my server and code peace")
 })
 app.listen(3000  , function (err) {
     if (err) console.error(err);
