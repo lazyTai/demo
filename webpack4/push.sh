04b40178822fef85b41e27226ac0729a9336571b
lazyTai <1352983234@qq.com>
2018-05-10 18:00:14 +0800

    git auto commit

--- webpack4/push.sh
+++ webpack4/push.sh
@@ -3,6 +3,8 @@ git pull
 git add .
 git commit -a -m 'git auto commit'
 git log -1
+git remove add origin https://github.com/lazyTai/demo.git
 git push -u origin master
+git push
 git remote -v
 git status
